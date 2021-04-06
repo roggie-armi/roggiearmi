8383228486ad635f4d287078b506406803100003
roggie-armi <82038887+roggie-armi@users.noreply.github.com>
2021-04-06 23:37:23 +0700

    Add files via upload

---
+++ method.java
@@ -0,0 +1,10 @@
+package Classes_and_object;
+
+public class method {
+    public static void main(String[] args) {
+        hello();
+    }
+    public static void hello() {
+        System.out.println("hi!");
+    }
+}
---
+++ the_return_type.java
@@ -0,0 +1,11 @@
+package Classes_and_object;
+
+public class the_return_type {
+    public static void main(String[] args) {
+        int x = 10;
+        test(x);
+    }
+    static void test(int x){
+        System.out.println(x);
+    }
+}
